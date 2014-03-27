7f4e1d3d42167aa8fc20de68ac1ed86e49650b73
Jonathanster <jonathan.young@mail.utoronto.ca>
2014-03-27 17:56:12 -0400

    Create quiz_model.php

---
+++ application/models/quiz_model.php
@@ -0,0 +1,23 @@
+<?php
+  class Quiz_model extends CI_MODEL{
+  	function create_profile()
+  	{
+  		$data['id'] = $this->input->post('id');
+  		$data['name'] = $this->input->post('name');
+  		$data['age'] = $this->input->post('age');
+  		$data['email'] = $this->input->post('email');
+  		$data['result'] = $this->input->post('result');
+  	}
+  	
+  	//Fix this function, not fully functional
+  	function search_result()
+  	{
+  		$search_term = $this->input->post('searchquery');
+  		$sql = "SELECT * FROM quizData WHERE MATCH (id) AGAINST ('" . $search_term . "')";
+		$query = $this->db->query($sql);
+		return $query->result_array();
+  	}
+  
+  	
+
+  }
