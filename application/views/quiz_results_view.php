25b2b1f37137cdff7bc328f32d13382385a149a2
Jonathanster <jonathan.young@mail.utoronto.ca>
2014-03-30 22:13:59 -0400

    Update quiz_results_view.php

--- application/views/quiz_results_view.php
+++ application/views/quiz_results_view.php
@@ -4,6 +4,9 @@
 	<body>
 		<h2>Profile created!</h2>
 
+			<iframe width="560" height="315" src="//www.youtube.com/embed/y6Sxv-sUYtM" frameborder="0" allowfullscreen></iframe>
+			This page needs to show the results, perhaps user information, etc.
+			
 			<form action="http://phoenix.sheridanc.on.ca/~ccit1585/suitUpFP/index.php/quiz">
     			<input type="submit" value="Go to Quiz">
 			</form>
