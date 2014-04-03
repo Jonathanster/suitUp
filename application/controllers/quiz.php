85a402002a63cc139e1efaf1cf738e4c7cd9fa90
Jonathanster <younjona@oa-j318-29.ddi.sheridanc.on.ca>
2014-04-03 16:11:55 -0400

    figureditoutyo

--- application/controllers/quiz.php
+++ application/controllers/quiz.php
@@ -29,29 +29,26 @@
 			$this->load->helper('form');
 
 			
-// First, go through all the questions:
-$points = 0;
-foreach($_POST as $quizanswer) {
-    $points = $points + $quizanswer;
-}
+			$points = 0;
+			foreach($_POST as $quizanswer) {
+			    $points = $points + $quizanswer;
+			}
 
-// Now points is the total of all field values.  Let's see where we want to go.
-if ($points > 6) {
-    echo 'david';
-} else if ($points > 11) {
-    echo ' jon';
-} else if ($points > 16) {
-    echo ' Amy';
-} else {
-    echo ' Hana';
-}
+			// Now points is the total of all field values.  Let's see where we want to go.
+			if ($points < 11) {
+			    $data['title'] = "Suit Up Quiz!";
+				$this->quiz_model->save_answers();
+				$this->load->view('templates/header_quiz', $data);
+				$this->load->view('quiz_results_view');
+				$this->load->view('templates/footer_quiz', $data);	
+			} else if ($points <16) {
+			    echo ' jon';
+			} else if ($points <20) {
+			    echo ' Amy';
+			} else {
+			    echo ' Hana';
+			}
 
-
-			//$data['title'] = "Suit Up Quiz!";
-			//$this->quiz_model->save_answers();
-			//$this->load->view('templates/header_quiz', $data);
-			//$this->load->view('quiz_results_view');
-			//$this->load->view('templates/footer_quiz', $data);	
 		}
 
 	}
