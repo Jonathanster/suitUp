975042762d00af5383aa5d95340c5526b520fb08
hanaelali <hana.elali@mail.utoronto.ca>
2014-04-06 15:29:42 -0400

    Update classic.php

--- application/views/classic.php
+++ application/views/classic.php
@@ -1,8 +1,7 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="/~ccit1585/suitUpFP/assets/style.css">
-<link href='http://fonts.googleapis.com/css?family=Allerta+Stencil' rel='stylesheet' type='text/css'>
-Your Result is: Classic!
+
 </head>
 
 <body>
@@ -11,6 +10,7 @@ Your Result is: Classic!
 <div id = "image"> <img src="http://images-ff.asos-media.com/shmotterstorage/14696/outfit_large_22e40862-4ee6-41bf-934c-dbbb7efcab06.jpg" alt="The Classic Outfit"> </div>
 
  <div id = "philosophy">
+
   <h2>Your style philosophy:</h2>
     <p> Don't mess with a good thing. This doesn't mean your look is boring-it means you know what works. Sure, flair is fine once in a while, but you know not to depend on trends when you can make a statement using nothing but sophistication and refinement.</p>
   </div>
