9cfc47fcd990c3137d970ec5ec8acbb29eb33447
hanaelali <hana.elali@mail.utoronto.ca>
2014-04-06 15:32:53 -0400

    Update rugged.php

--- application/views/rugged.php
+++ application/views/rugged.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-Your Result is: Rugged!
 <link rel="stylesheet" type="text/css" href="/~ccit1585/suitUpFP/assets/style.css">
 </head>
 
@@ -9,17 +8,20 @@ Your Result is: Rugged!
 
 <div id= "image"><img src="http://images-ff.asos-media.com/shmotterstorage/20562/outfit_large_80b24aa2-020f-478a-900d-e3ba23e4cea1.jpg" alt="The Rugged Outfit">  </div>
 
-<p> Your style philosophy: No one can rival your style-not that anyone would try. Whether you're wearing a structured denim jacket to a meeting or ripped jeans on a date, your look has just as much attitude as you do.</p>
+<div id="philosophy"> 
+  <h2> Your style philosophy: </h2>
+  <p> No one can rival your style-not that anyone would try. Whether you're wearing a structured denim jacket to a meeting or ripped jeans on a date, your look has just as much attitude as you do.</p>
+</div>
 
   <div id= "list"> 
 <h2> Shop this look: </h2>
  
 <div id = "links"> 
-  <ol>
- <li> <a href="http://levi.com"> Levi's </a> </li>
- <li> <a href="http://www.express.com"> Express </a></li>
- <li> <a href="http://shop.timberland.com/category/index.jsp?categoryId=3976196"> Timberland </a></li>
- </ol>
+    <ol>
+  <li> <a href="http://levi.com"> Levi's </a> </li>
+  <li> <a href="http://www.express.com"> Express </a></li>
+  <li> <a href="http://shop.timberland.com/category/index.jsp?categoryId=3976196"> Timberland </a></li>
+    </ol>
  
  </div>
  </div>
