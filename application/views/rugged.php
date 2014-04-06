2c8a054e032c3f8528bb9fc7b4fa7c3e0918d6e0
hanaelali <hana.elali@mail.utoronto.ca>
2014-04-06 15:25:22 -0400

    Update rugged.php

--- application/views/rugged.php
+++ application/views/rugged.php
@@ -9,7 +9,7 @@ Your Result is: Rugged!
 
 <div id= "image"><img src="http://images-ff.asos-media.com/shmotterstorage/20562/outfit_large_80b24aa2-020f-478a-900d-e3ba23e4cea1.jpg" alt="The Rugged Outfit">  </div>
 
-<p> Your style philosophy: No one can rival your style-not that anyone would try. Whether youre wearing a structured denim jacket to a meeting or ripped jeans on a date, your look has just as much attitude as you do.</p>
+<p> Your style philosophy: No one can rival your style-not that anyone would try. Whether you're wearing a structured denim jacket to a meeting or ripped jeans on a date, your look has just as much attitude as you do.</p>
 
   <div id= "list"> 
 <h2> Shop this look: </h2>
