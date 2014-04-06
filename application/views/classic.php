73a07fbb61de51f0807027a9922ed153c6e608fc
amycsmith <amycsmith90@gmail.com>
2014-04-06 15:01:02 -0400

    Update classic.php

--- application/views/classic.php
+++ application/views/classic.php
@@ -1,27 +1,31 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="/~ccit1585/suitUpFP/assets/style.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href='http://fonts.googleapis.com/css?family=Allerta+Stencil' rel='stylesheet' type='text/css'>
 Your Result is: Classic!
 </head>
 
 <body>
 <h1> Your Style is: Classic! </h1>
 
-<div id= image> </div><img src="http://images-ff.asos-media.com/shmotterstorage/14696/outfit_large_22e40862-4ee6-41bf-934c-dbbb7efcab06.jpg" alt="The Classic Outfit"> </div>
+<div id = image> <img src="http://images-ff.asos-media.com/shmotterstorage/14696/outfit_large_22e40862-4ee6-41bf-934c-dbbb7efcab06.jpg" alt="The Classic Outfit"> </div>
 
-<p> Your style philosophy: Don't mess with a good thing. This doesn't mean your look is boring-it means you know what works. Sure, flair is fine once in a while, but you know not to depend on trends when you can make a statement using nothing but sophistication and refinement.</p>
+ <div id = philosophy>
+  <h2>Your style philosophy:</h2>
+    <p> Don't mess with a good thing. This doesn't mean your look is boring-it means you know what works. Sure, flair is fine once in a while, but you know not to depend on trends when you can make a statement using nothing but sophistication and refinement.</p>
+  </div>
 
-<div id= links> 
-<h2> Shop this look: </h2>
+<div id= links>
+  <h2> Shop this look: </h2>
  
- <div id= list> 
-  <ol>
- <li> <a href="http://www.tommy.com"> Tommy Hilfiger </a> </li>
- <li> <a href="http://www.kennethcole.com"> Kenneth Cole </a></li>
- <li> <a href="http://www.thomaspink.com"> Thomas Pink </a></li>
- </ol>
+  <div id= list>
+    <ol>
+      <li> <a href="http://www.tommy.com"> Tommy Hilfiger </a> </li>
+       <li> <a href="http://www.kennethcole.com"> Kenneth Cole </a></li>
+      <li> <a href="http://www.thomaspink.com"> Thomas Pink </a></li>
+    </ol>
  </div>
- </div>
+</div>
   
 
   
