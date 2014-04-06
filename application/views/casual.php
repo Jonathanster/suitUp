513cad875343965cfb180d4e377a1fe11acc4541
Dlin7 <david.lin.2012@gmail.com>
2014-04-06 17:59:39 -0400

    Update casual.php

--- application/views/casual.php
+++ application/views/casual.php
@@ -1,38 +1,56 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="/~ccit1585/suitUpFP/assets/style.css">
-<link href='http://fonts.googleapis.com/css?family=Allerta+Stencil' rel='stylesheet' type='text/css'>
 </head>
 
 <body>
 <h1> Your Style is: Casual! </h1>
 
+
+
+<div id="wrapper">
+	<p>fakslfjasdf</p>
+			<div id="philosophy">
+				<h2>Your style philosophy: </h2>
+				<p> You're an on-the-go straight-shooter and everyone can tell by your wardrobe. Sure, you can't imagine wearing something uncomfortable, but that doesn't mean you spend your days in sweats. You know how to make the most of basics to create a look that's structured yet approachable.</p>
+					<div id= "backbutton">  <form method="get" action="http://phoenix.sheridanc.on.ca/~ccit1585/suitUpFP/index.php/quiz/create"><button type="submit"> Take Quiz Again </button></form>
+					</div>
+
+
+
+			
+
+
+
+
+
+			</div>
+
+
+					<div id ="links">
+						<h2> Shop this look: </h2>
+		 
+		  
+							 <div id = "list"> 
+							 <ol>
+							 <li> <a href="http://www.nautica.com"> Nautica </a> </li>
+							 <li> <a href="http://www.clubmonaco.com"> Club Monaco</a></li>
+							 <li> <a href="http://www.converse.com"> Converse Shoes </a></li>
+							 </ol>
+		 
+		 			</div>
+			
+
+</div>
+ 
 <div id="image"> <img src="http://www.itsnotforgirls.com/wp-content/uploads/2012/08/semi-casual-style-men.jpg" alt="The Casual Outfit"></div>
 
-<div id= "philosophy">
-<h2>Your style philosophy: </h2>
-<p> You're an on-the-go straight-shooter and everyone can tell by your wardrobe. Sure, you can't imagine wearing something uncomfortable, but that doesn't mean you spend your days in sweats. You know how to make the most of basics to create a look that's structured yet approachable.</p>
-</div>
-
-<div id ="links">
-<h2> Shop this look: </h2>
- 
-  
- <div id = "list"> 
- <ol>
- <li> <a href="http://www.nautica.com"> Nautica </a> </li>
- <li> <a href="http://www.clubmonaco.com"> Club Monaco</a></li>
- <li> <a href="http://www.converse.com"> Converse Shoes </a></li>
- </ol>
- 
- </div>
- </div>
- 
-<div id= "backbutton">  <form method="get" action="http://phoenix.sheridanc.on.ca/~ccit1585/suitUpFP/index.php/quiz/create"><button type="submit"> Take Quiz Again </button></form>
-</div>
   
 
   
 </body>
+
+</html>
+
 
 </html>
