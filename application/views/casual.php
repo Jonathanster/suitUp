997995bfa88467245f5035c446171c4884b58279
hanaelali <hana.elali@mail.utoronto.ca>
2014-04-06 15:28:46 -0400

    Update casual.php

--- application/views/casual.php
+++ application/views/casual.php
@@ -1,7 +1,6 @@
 <html>
 <head>
 <link rel="stylesheet" type="text/css" href="/~ccit1585/suitUpFP/assets/style.css">
-Your Result is: Casual!
 </head>
 
 <body>
@@ -9,7 +8,9 @@ Your Result is: Casual!
 
 <div id="image"> <img src="http://www.itsnotforgirls.com/wp-content/uploads/2012/08/semi-casual-style-men.jpg" alt="The Casual Outfit"></div>
 
+<div id= "philosophy">
 <p> Your style philosophy: You're an on-the-go straight-shooter and everyone can tell by your wardrobe. Sure, you can't imagine wearing something uncomfortable, but that doesn't mean you spend your days in sweats. You know how to make the most of basics to create a look that's structured yet approachable.</p>
+</div>
 
 <div id ="links">
 <h2> Shop this look: </h2>
@@ -21,6 +22,7 @@ Your Result is: Casual!
  <li> <a href="http://www.clubmonaco.com"> Club Monaco</a></li>
  <li> <a href="http://www.converse.com"> Converse Shoes </a></li>
  </ol>
+ 
  </div>
  </div>
  
