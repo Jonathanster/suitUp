80fa804b983c26f2507c2d0a1b2b41f8069f83a0
Jonathanster <jonathanyoung@utmuser150-102.wireless.utoronto.ca>
2014-04-07 16:01:27 -0400

    idk

---
+++ application/views/past_result_view.php
@@ -0,0 +1,5 @@
+
+<a href="/~ccit1585/suitUpFP/index.php/quiz/update/<?php echo $this->input->post('idNum'); ?>
+">Let's test this my friends!</a>
+<?php echo $this->input->post('idNum'); ?>
+<p>derpa</p>
