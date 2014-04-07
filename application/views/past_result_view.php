e4a0a6ba2850d621accf7810e8820b726e8c50b5
Jonathanster <jonathanyoung@utmuser150-102.wireless.utoronto.ca>
2014-04-07 17:28:47 -0400

    changed stuff

--- application/views/past_result_view.php
+++ application/views/past_result_view.php
@@ -1,5 +1,7 @@
 
-<a href="/~ccit1585/suitUpFP/index.php/quiz/update/<?php echo $this->input->post('idNum'); ?>
-">Let's test this my friends!</a>
-<?php echo $this->input->post('idNum'); ?>
-<p>derpa</p>
+<a id="extraLink" href="/~ccit1585/suitUpFP/index.php/quiz/update/<?php echo $this->input->post('idNum'); ?>
+">Click here to see your profile and result!</a>
+<br>
+<br>
+<br>
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src="/~ccit1585/suitUpFP/assets/trip.jpg"></a></p>
--- application/views/profile_begin_view.php
+++ application/views/profile_begin_view.php
@@ -14,23 +14,34 @@
 
 
 		<?php echo form_open('quiz/create') ?>
-		<p>Begin your quiz!</p>
+		<h3>Begin your quiz!</h3>
 		<input type="submit" name="submit" value="Create">
 			</form>
 
-			<h2> View Past Results</h2>
-			<h3>Please enter your ID Number</h3>
+			<p> View Past Results</p>
+			<p>Please enter your ID Number</p>
 
 				<?php echo form_open('quiz/viewPast') ?>
 				<input type="int" name="idNum">
 				<input type="submit" name="submit" value="Create">
-			</form>
+				</form>
 	</div>
 
 		<div class="about">
 		<p ID="group">
-			About the creators:
-		</p>
+About the website:
+</p>
+<p ID="idk">
+Guys, confused on what to wear? Suit Up is here to help!
+Take our online quiz to find out what your fashion style is. We offer a personalized style philosophy as well as suggested stores to shop your look. Want to check back later? We save your result so you can check back anytime.
+</p>
+<p ID="group">
+About the creators:
+</p>
+<p ID="idk">
+We are a team of four CCIT students studying at UTM. Our inspiration for this project stems from our interest in fashion and clothing. We understand the challenges of not knowing what to wear and where to shop, so we decided to create an online styling website.
+- Jonathan, David, Hana & Amy		</p>
+<img ID="fashionGuy" src="/~ccit1585/suitUpFP/assets/fashionguy.jpg">
 		</div>
 
 </div>
