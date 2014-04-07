9c6e25329ef309c42ed9e82cf9f9f89dff12075a
Jonathanster <jonathanyoung@utmuser150-102.wireless.utoronto.ca>
2014-04-07 15:59:11 -0400

    deleted blog files

--- application/views/templates/Blog Files/header_admin.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-
-<html>
-	<head>
-		<title><?php echo $title; ?></title>
-		<link rel="stylesheet" type="text/css" href="/~ccit1585/cct460/assets/style_admin.css">
-	</head>
-		
-	<body>
--- application/views/templates/Blog Files/header_blog.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-	<html>
-		<head>
-			<title><?php echo $title; ?></title>
-			<link rel="stylesheet" type="text/css" href="/~ccit1585/cct460/assets/style_blog.css">
-
-		</head>
-		
-	<body>
